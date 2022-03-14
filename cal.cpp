cf7e6e40b801b589a36359dee7186158f15a2a88
AsawaDevesh <deveshasawa999@gmail.com>
2022-03-14 09:47:21 +0000

    Commited

---
+++ cal.cpp
@@ -0,0 +1,29 @@
+//Program 
+#include<iostream>
+using namespace std;
+class Cal{
+public:
+int a,b;
+void setinput(int x,int  y)
+{
+a=x;
+b=y;
+}
+int add()
+{
+return a+b;
+}
+};
+int main()
+{
+Cal obj;
+int x,y;
+cout<<"Enter Number 1 : ";
+cin>>x;
+cout<<"Enter Number 1 : ";
+cin>>y;
+obj.setinput(x,y);
+cout<<"Inputs are : "<<obj.a<<" "<<obj.b << endl;
+cout<<"Sum : "<<obj.add()<< endl;
+return 0;
+}
